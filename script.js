18eccf2d9b3f478cb1e8947f004e1a6517cd3398
feruzaxabibova99 <feruzaaa1007@gmail.com>
2021-09-10 08:29:55 +0300

    Add site HTML codes

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-var modal = document.getElementById('myModal');
+ var modal = document.getElementById('myModal');
  var btn = document.getElementById('myBtn');
  var span = document.getElementsByClassName('close')[0];
  var val = document.getElementById("input-in");
  var send = document.getElementById("send");
- var result = document.getElementById("str");
+ var result = document.querySelector('.class-js');
  var ageNormal = 18;
 
  btn.onclick = function () {
@@ -14,17 +14,19 @@ var modal = document.getElementById('myModal');
  	modal.style.display = 'none';
  }
 
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function () {
-modal.style.display = "none";
-}
+// var span = document.getElementsByClassName("close")[0];
+// 	span.onclick = function () {
+// 	modal.style.display = "none";
+// }
 
 send.onclick = function() {
 	var val = document.getElementById("input-in").value;
 
 	if (val >= ageNormal) {
+		result.style.backgroundColor = 'green';
 		document.getElementById("str").innerHTML = "We wish you an interesting holiday!";
 	}else {
+		result.style.backgroundColor = 'red';
 		document.getElementById("str").innerHTML = "Your age is not suitable!";
 	}
 }
